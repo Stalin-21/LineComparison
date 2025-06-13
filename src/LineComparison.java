57504ca01f2b24319daaa80f7bcbd6c20667742a
stalin-21 <stalinguna21@gmail.com>
2025-06-13 12:54:05 +0530

    UC2 - Implemented logic for checking both line lengths.

--- src/LineComparison.java
+++ src/LineComparison.java
@@ -2,11 +2,24 @@ public class LineComparison {
     public static void main(String[] args) {
         System.out.println("Welcome to Line Comparison computation program ");
 
-        int x1 = 1, y1 = 2;
-        int x2 = 4, y2 = 6;
+        // First line points
+        double x1 = 1, y1 = 2;
+        double x2 = 4, y2 = 6;
 
-        double length = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-        System.out.println("The length of the line is: " + length);
+        // Second line points
+        double a1 = 0 , b1 = 0;
+        double a2 = 3 , b2 = 4;
 
+        Double length1 = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+        Double length2 = Math.sqrt(Math.pow(a2 - a1, 2) + Math.pow(b2 - b1, 2));
+
+        System.out.println("First line length is " + length1);
+        System.out.println("Second line length is " + length2);
+
+        if(length1.equals(length2)){
+            System.out.println("Lines are equal");
+        }else {
+            System.out.println("Lines are not equal");
+        }
     }
 }
