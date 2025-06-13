c3cc6e2e1c74b6ace8c214917e69ecab3e631bf9
stalin-21 <stalinguna21@gmail.com>
2025-06-13 14:24:07 +0530

    UC4: Refactored code using OOP - Added Point and Line classes with equals() and compareTo()

--- src/LineComparison.java
+++ src/LineComparison.java
@@ -1,31 +1,55 @@
+class Point {
+    double x, y;
+    Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+class Line implements Comparable<Line> {
+    Point start, end;
+
+    Line(Point start, Point end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    double getLength() {
+        return Math.sqrt(Math.pow(end.x - start.x, 2) + Math.pow(end.y - start.y, 2));
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof Line)) return false;
+        Line other = (Line) obj;
+        return Double.compare(this.getLength(), other.getLength()) == 0;
+    }
+
+    @Override
+    public int compareTo(Line other) {
+        return Double.compare(this.getLength(), other.getLength());
+    }
+}
+
 public class LineComparison {
     public static void main(String[] args) {
         System.out.println("Welcome to Line Comparison computation program ");
 
-        // First line points
-        double x1 = 1, y1 = 2;
-        double x2 = 4, y2 = 6;
+        Line line1 = new Line(new Point(1, 2), new Point(4, 6));
+        Line line2 = new Line(new Point(0, 0), new Point(3, 4));
 
-        // Second line points
-        double a1 = 0 , b1 = 0;
-        double a2 = 3 , b2 = 4;
+        System.out.println("Line1 length: " + line1.getLength());
+        System.out.println("Line2 length: " + line2.getLength());
 
-        Double length1 = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-        Double length2 = Math.sqrt(Math.pow(a2 - a1, 2) + Math.pow(b2 - b1, 2));
+        System.out.println("Are lines equal? " + line1.equals(line2));
 
-        // compare line length
-
-        int result = length1.compareTo(length2);
-
-        System.out.println("First line length is " + length1);
-        System.out.println("Second line length is " + length2);
-
-        if(result == 0){
-            System.out.println("Lines are equal");
-        }else if(result > 0) {
-            System.out.println("Line 1 is longer than Line2");
-        }else {
-            System.out.println("Line 2 is shorter than Line1");
-        }
+        int comparison = line1.compareTo(line2);
+        if (comparison > 0)
+            System.out.println("Line1 is longer");
+        else if (comparison < 0)
+            System.out.println("Line2 is longer");
+        else
+            System.out.println("Both lines are equal");
     }
 }
