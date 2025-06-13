ed453251119f1fdd27f208156f281d3ecb4ed3c3
stalin-21 <stalinguna21@gmail.com>
2025-06-13 11:29:33 +0530

    Added welcome message in master

--- src/LineComparison.java
+++ src/LineComparison.java
@@ -1,5 +1,5 @@
 public class LineComparison {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Welcome to LineComparison computation program ");
     }
 }
